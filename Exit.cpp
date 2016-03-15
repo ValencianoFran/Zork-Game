a376b01e277c6b6ed0e26b560ecc43982ed67d5b
Fran <fran152job@gmail.com>
2016-03-15 17:54:30 +0100

    Created Exits and fixed player error

--- Exit.cpp
+++ Exit.cpp
@@ -3,3 +3,38 @@
 #include <string.h>
 #include "exit.h"
 #include "room.h"
+
+Exit::Exit()
+{}
+
+Exit::Exit(const char* _name, const char* desc, Room* orig, Room* dest, const dir direc)
+{
+	if (_name != NULL && desc != NULL && orig != NULL && dest != NULL)
+	{
+		strcpy_s(name, _name);
+		strcpy_s(description, desc);
+		origin = orig;
+		destination = dest;
+		direction = direc;
+	}
+}
+
+
+Exit::~Exit()
+{}
+
+bool Exit::Init(const char* _name, const char* desc, Room* orig, Room* dest, const dir direc)
+{
+	bool ret = true;
+
+	if (ret = (_name != NULL && desc != NULL && orig != NULL && dest != NULL))
+	{
+		strcpy_s(name, _name);
+		strcpy_s(description, desc);
+		origin = orig;
+		destination = dest;
+		direction = direc;
+	}
+
+	return ret;
+}
--- World.cpp
+++ World.cpp
@@ -9,10 +9,13 @@
 #include "room.h"
 #include "exit.h"
 
+#define NUM_EXITS 16
+
 World::World()
 {
 	room = new Room[9];     // 9 rooms
 	player = new Player; // 1 player and 4 npc
+	exits = new Exit[NUM_EXITS];
 }
 
 World::~World()
@@ -58,6 +61,61 @@ void World::CreateWorld()
 	room[6].Init("House", "This house is so extravagant\nThere's a man\n");
 	room[7].Init("Shop", "Maybe i should buy something to kill the sharks and leave this islands\n");
 	room[8].Init("Store", "Here is the inventory of the shop, I could catch something borrowed");
+
+	/*  -- EXITS --    */
+	//Shore - Palmtrees Island
+
+	exits[0].Init("Palmtrees Island\n", "There are a lot of trees, seems interesting\n", room, (room + 3), north);
+
+	//Palmtrees Island - Shore
+	exits[1].Init("Shore\n", "There is where I begin this adventure\n", (room + 3), (room), south);
+
+	//Palmtrees Island - Monkey
+	exits[2].Init("Palmtree\n", "Maybe there are something in the top of that palmtree\n", (room + 3), (room + 4), east);
+
+	//Monkey - Palmtrees Island
+	exits[3].Init("Palmtrees Island\n", "There are a lot of trees, seems interesting\n", (room + 4), (room + 3), west);
+
+	//Shore - Sea
+	exits[4].Init("Palmtree\n", "Maybe there are something in the top of that palmtree\n", (room + 3), (room + 4), east);
+
+	//Sea - Shore
+	exits[5].Init("Shore\n", "\n", (room + 2), (room), east);
+
+	//Shore - Depths
+	exits[6].Init("Depths\n", "\n", (room), (room + 1), down);
+
+	//Depths - Shore
+	exits[7].Init("Shore\n", "\n", (room + 1), (room), up);
+
+	//Shore - Outside House
+	exits[8].Init("Outside House\n", "\n", (room), (room + 5), east);
+
+	//Outside House - Shore
+	exits[9].Init("Shore\n", "\n", (room + 5), (room), west);
+
+	//Shore - Shop
+	exits[10].Init("Shop\n", "\n", (room), (room + 7), south);
+
+	//Shop - Shore
+	exits[11].Init("Shore\n", "\n", (room + 7), (room), north);
+
+	//Shop - Store
+	exits[12].Init("Store\n", "\n", (room + 7), (room + 8), down);
+
+	//Store - Shop
+	exits[13].Init("Shop\n", "\n", (room + 8), (room + 7), up);
+
+	//Outside House - House
+	exits[14].Init("House\n", "\n", (room + 5), (room + 6), south);
+	exits[14].gate = true;
+	exits[14].close = true;
+
+	//House - Outside House
+	exits[15].Init("Outside House\n", "\n", (room + 6), (room + 5), north);
+
+
+	CreatePlayer();
 }
 
 
